9eec6b15ac7f7da9e44e30fb631692cd2b4ead9a
PTalluru <talluruprasanthkumar04071995@gmail.com>
2022-12-16 22:39:26 +0530

    Assignment 4 updated but not completed

---
+++ Assignment_4/JdbcAssignment2/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+/*
+Question2
+perform insertion opertion and also perform retrieval operation on the following
+data
+name =>
+address=>
+gender  =>
+DOB DOJ DOM
+=> dd-MM-yyyy
+=> MM-dd-yyyy
+=> yyyy-MM-dd
+*/
+package org.example;
+/**
+ * @author PTalluru
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
